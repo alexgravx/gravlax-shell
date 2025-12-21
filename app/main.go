8864d2515735628941c2fe900437bc212b565999
Alexandre Gravereaux <117570987+alexgravx@users.noreply.github.com>
2025-12-21 14:39:57 +0100

    [ADD] executable paths in type for non builtins

--- app/main.go
+++ app/main.go
@@ -31,12 +31,37 @@ func format_command(commands string) (string, []string) {
 	return command, args
 }
 
-func is_command(command string) bool {
+func is_builtin(command string) bool {
 	switch command {
 	case "echo", "exit", "type":
 		return true
 	}
 	return false
+}
+
+func exec_path(path string) bool {
+	file, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	mode := file.Mode()
+	if mode&0o111 != 0 {
+		return true
+	}
+	return false
+}
+
+func is_exec(command string) (bool, string) {
+	path := os.Getenv("PATH")
+	dirs := strings.SplitSeq(path, string(os.PathListSeparator))
+	for dir := range dirs {
+		filePath := dir + string(os.PathSeparator) + command
+		exec := exec_path(filePath)
+		if exec {
+			return true, filePath
+		}
+	}
+	return false, ""
 }
 
 func shell() {
@@ -54,10 +79,15 @@ func shell() {
 		fmt.Println(strings.Join(args, " "))
 	case "type":
 		type_command := args[0]
-		if is_command(type_command) {
+		if is_builtin(type_command) {
 			fmt.Println(type_command + " is a shell builtin")
 		} else {
-			fmt.Println(type_command + ": not found")
+			exec, path := is_exec(type_command)
+			if exec {
+				fmt.Println(type_command + " is " + path)
+			} else {
+				fmt.Println(type_command + ": not found")
+			}
 		}
 	default:
 		// Prints the "<command>: command not found" message
@@ -66,7 +96,7 @@ func shell() {
 }
 
 func main() {
-	for true {
+	for {
 		shell()
 	}
 }
