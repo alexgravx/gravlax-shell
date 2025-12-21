d2a9b4395f4c4b32b6582bce23f3678fca521ac3
Alexandre Gravereaux <117570987+alexgravx@users.noreply.github.com>
2025-12-21 11:16:43 +0100

    [ADD] echo command

--- app/main.go
+++ app/main.go
@@ -4,22 +4,46 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func shell() {
-	// Shell prompt
-	fmt.Print("$ ")
-	// Captures the user's command
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+func read_input() string {
+	commands, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error reading input:", err)
 		os.Exit(1)
 	}
-	// Format command, remove trailing linespace
-	command = command[:len(command)-1]
+	return commands
+}
+
+func format_command(commands string) (string, []string) {
+	// Remove trailing linespace
+	commands = commands[:len(commands)-1]
+	// Split command and args
+	command_args := strings.Split(commands, " ")
+	command := command_args[0]
+	var args []string
+	if len(command_args) > 1 {
+		args = command_args[1:]
+	} else {
+		args = []string{""}
+	}
+	return command, args
+}
+
+func shell() {
+	// Shell prompt
+	fmt.Print("$ ")
+	// Read command
+	commands := read_input()
+	// Format command
+	command, args := format_command(commands)
+	// Evaluate command
 	switch command {
 	case "exit":
 		os.Exit(0)
+	case "echo":
+		fmt.Println(strings.Join(args, " "))
 	default:
 		// Prints the "<command>: command not found" message
 		fmt.Println(command + ": command not found")
